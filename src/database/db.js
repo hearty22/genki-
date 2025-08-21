01daee08c1b0d39a679c4d5e489c49eca4d2bd72
hearty_dev <facudnoesclante@gmail.com>
2025-08-21 16:37:41 -0300

    style: interfaz visual para los inicios de sesion y registros

--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,7 @@ import { sequelize } from "./database.js";
 
 export const db_conect = async ()=>{
     try {
-        await sequelize.sync({force: true, alter: true});
+        await sequelize.sync({force: true});
         console.log("servidor conectado con la base de datos");
     } catch (error) {
         console.log("conexion fallida con la base de datos");
